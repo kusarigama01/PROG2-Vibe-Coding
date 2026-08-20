780cfc6f79605d6274e30b628308d6ab68f58468
RANDRIAMANAMBINA Sammy Mathieu <sam.mathieurandria@gmail.com>
2026-08-20 14:28:28 +0300

    initial commit

---
+++ src/test/java/PromptMafanaFo/n1/N1ApplicationTests.java
@@ -0,0 +1,13 @@
+package PromptMafanaFo.n1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
